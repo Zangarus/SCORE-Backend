dce09f28da27b144c1b4e6602961a54e32c37a79
Marwin Madsen <marwin.madsen@gmail.com>
2021-04-16 23:37:00 +0200

    add user entity and child

---
+++ src/entry.entities.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+export enum TravelType {
+    FOOT,
+    BIKE,
+    CAR,
+    TRAIN,
+    PLANE
+}
+
+@Entity()
+export class Entry {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    distance: number;
+
+    @Column()
+    travelType: TravelType;
+
+    @CreateDateColumn({
+        precision: null,
+        type: 'timestamp',
+        default: () => 'CURRENT_TIMESTAMP',
+    })
+    eintragsdatum?: Date;
+}
---
+++ src/user.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
+import { Entry } from 'src/entry.entities';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  score: number;
+
+  @OneToOne(() => Entry, {
+    cascade: true
+  })
+  @JoinColumn()
+  entries: Entry[];
+}

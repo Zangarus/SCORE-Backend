d059f25c8072a85644683e3fcc11af1b0571cbbd
Marwin Madsen <marwin.madsen@gmail.com>
2021-04-17 02:08:32 +0200

    fix missing import

--- src/entry/entry.module.ts
+++ src/entry/entry.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EntryService } from './entry.service';
 import { EntryController } from './entry.controller';
+import { Entry } from './entry.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Entry])],
   providers: [EntryService],
   controllers: [EntryController]
 })
-export class EntryModule {}
+export class EntryModule { }
